392876110e489db520820b8b36e32b39072d52a2
BigtoC <taitocdt@gmail.com>
2026-04-26 16:38:52 +0800

    refactor(box_shadow): format code for better readability

--- lib/core/utils/box_shadow.dart
+++ lib/core/utils/box_shadow.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'color_palette.dart';
 
 BoxShadow buildBoxShadow(double blurRadius) {
-  final int colorIndex = DateTime.now().millisecondsSinceEpoch % kCategoryColorPalette.length;
+  final int colorIndex =
+      DateTime.now().millisecondsSinceEpoch % kCategoryColorPalette.length;
   final Color color = kCategoryColorPalette[colorIndex];
   return BoxShadow(
     color: color.withAlpha(31),
