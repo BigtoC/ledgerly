84cec8754a3ec44a6167ef2de9c58d8ca7276496
BigtoC <taitocdt@gmail.com>
2026-05-08 00:05:26 +0800

    feat(recurring-transactions): add repositories and use case for pending transactions and recurring rules

--- lib/app/providers/repository_providers.g.dart
+++ lib/app/providers/repository_providers.g.dart
@@ -169,5 +169,85 @@ final shoppingListRepositoryProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef ShoppingListRepositoryRef = ProviderRef<ShoppingListRepository>;
+String _$pendingTransactionRepositoryHash() =>
+    r'800478b460c3c65248a0a6b23188c1672be5f3b8';
+
+/// See also [pendingTransactionRepository].
+@ProviderFor(pendingTransactionRepository)
+final pendingTransactionRepositoryProvider =
+    Provider<PendingTransactionRepository>.internal(
+      pendingTransactionRepository,
+      name: r'pendingTransactionRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$pendingTransactionRepositoryHash,
+      dependencies: <ProviderOrFamily>[appDatabaseProvider],
+      allTransitiveDependencies: <ProviderOrFamily>{
+        appDatabaseProvider,
+        ...?appDatabaseProvider.allTransitiveDependencies,
+      },
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PendingTransactionRepositoryRef =
+    ProviderRef<PendingTransactionRepository>;
+String _$recurringRulesRepositoryHash() =>
+    r'f297f662435691a5d8d42b616551967de2993046';
+
+/// See also [recurringRulesRepository].
+@ProviderFor(recurringRulesRepository)
+final recurringRulesRepositoryProvider =
+    Provider<RecurringRulesRepository>.internal(
+      recurringRulesRepository,
+      name: r'recurringRulesRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$recurringRulesRepositoryHash,
+      dependencies: <ProviderOrFamily>[appDatabaseProvider],
+      allTransitiveDependencies: <ProviderOrFamily>{
+        appDatabaseProvider,
+        ...?appDatabaseProvider.allTransitiveDependencies,
+      },
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RecurringRulesRepositoryRef = ProviderRef<RecurringRulesRepository>;
+String _$recurringGenerationUseCaseHash() =>
+    r'350e388f51b864c88329360e1078cac008904655';
+
+/// Use-case provider. Lives here so that controllers in
+/// `lib/features/.../*_controller.dart` can `ref.read` the use case
+/// without importing `data/database/...` (forbidden by
+/// `controllers_forbid_db_and_services` in import_analysis_options.yaml).
+///
+/// Copied from [recurringGenerationUseCase].
+@ProviderFor(recurringGenerationUseCase)
+final recurringGenerationUseCaseProvider =
+    Provider<RecurringGenerationUseCase>.internal(
+      recurringGenerationUseCase,
+      name: r'recurringGenerationUseCaseProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$recurringGenerationUseCaseHash,
+      dependencies: <ProviderOrFamily>[
+        appDatabaseProvider,
+        recurringRulesRepositoryProvider,
+        pendingTransactionRepositoryProvider,
+      ],
+      allTransitiveDependencies: <ProviderOrFamily>{
+        appDatabaseProvider,
+        ...?appDatabaseProvider.allTransitiveDependencies,
+        recurringRulesRepositoryProvider,
+        ...?recurringRulesRepositoryProvider.allTransitiveDependencies,
+        pendingTransactionRepositoryProvider,
+        ...?pendingTransactionRepositoryProvider.allTransitiveDependencies,
+      },
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RecurringGenerationUseCaseRef = ProviderRef<RecurringGenerationUseCase>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
